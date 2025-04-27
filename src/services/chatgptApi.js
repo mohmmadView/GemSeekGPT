4dd7fbfa77ac29bb78e9b4aca8494269c34d1d3e
mohmmadView <mamad.rahgoshahi@gmail.com>
2025-04-27 07:11:38 +0330

    not fix bug gpt run create node.js server

--- src/services/chatgptApi.js
+++ src/services/chatgptApi.js
@@ -1,28 +1,40 @@
-import axios from "axios";
+import { OpenAI } from 'openai';
+// const CHATGPT_API_URL = "https://api.openai.com/v1";
+// const apikey =`${import.meta.env.VITE_OPENAI_API_KEY}`  // api - open Ai
 
+// console.log( new OpenAI(""));
 
-const CHATGPT_API_URL = "https://api.openai.com/v1/chat/completions";
-const CHATGPT_API_KEY =import.meta.env.VITE_OPENAI_API_KEY  // api - open Ai
-console.log(CHATGPT_API_KEY);
+// const client = new OpenAI(CHATGPT_API_KEY);
+
+// const response = await client.responses.create({
+//   model: "gpt-4.1",
+//   input: "Write a one-sentence bedtime story about a unicorn.",
+// });
+
+const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+console.log(apiKey);
+
+const client = new OpenAI({ apiKey ,
+   dangerouslyAllowBrowser: true
+});
+
+  const response = await client.responses.create({
+    model: "gpt-4.1",
+    input: "hi chatgpt me connect api you how are you "
+  });
+  console.log(response);
+
 
 export const sendMessageChatGPT = async (message) => {
-  try {
-    const response = await axios.post(
-      CHATGPT_API_URL,
-      {
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: message }],
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${CHATGPT_API_KEY}`,
-        },
-      }
-    );
-    return response.data.choices[0].message.content;
-  } catch (error) {
-    console.error("ChatGPT API Error:", error);
-    throw error;
-  }
+  // try {
+  //   const response = await openai.chat.completions.create({
+  //           model: "gpt-3.5-turbo",
+  //           messages: [{ role: "user", content: message }],
+  //       });
+  //               return console.log(response)
+
+  // } catch (error) {
+  //   console.error("ChatGPT API Error:", error.message);
+  //   throw error;
+  // }
 };
--- src/services/geminiApi.js
+++ src/services/geminiApi.js
@@ -2,7 +2,7 @@ import {GoogleGenerativeAI} from "@google/generative-ai"
 const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
  const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash"});
- console.log(GEMINI_API_KEY,"GEMINI",model);
+//  console.log(GEMINI_API_KEY,"GEMINI",model);
 
 export const sendMessageGemini = async (message) => {
   try {
